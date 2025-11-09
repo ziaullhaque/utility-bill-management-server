285fcf1c72c36418152b32b886db9b79c9b36683
ahzahed <muhammadziaullhaque00@gmail.com>
2025-11-09 11:17:18 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Utility Bill Server is running on port ${port}`);
+});
