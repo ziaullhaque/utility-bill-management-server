7f0b888f4ce73f2e9bf90a514b83f982c3a80430
ahzahed <muhammadziaullhaque00@gmail.com>
2025-11-12 22:58:21 +0600

    added completed project

--- index.js
+++ index.js
@@ -100,7 +100,7 @@ async function run() {
       res.send(result);
     });
 
-    // save new payment
+    // Save New payment
     app.post("/payments", async (req, res) => {
       const payment = req.body;
       const result = await paymentsCollection.insertOne(payment);
